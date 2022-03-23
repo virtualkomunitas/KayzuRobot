8eed90ad8ea5cd443a5a3cdcaf1e8a50c0fc6517
virtualkomunitas <101849803+virtualkomunitas@users.noreply.github.com>
2022-03-23 18:10:47 +0700

    Update alive.py

--- SkyzuRobot/modules/alive.py
+++ SkyzuRobot/modules/alive.py
@@ -9,22 +9,22 @@ from SkyzuRobot.events import register
 from SkyzuRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/d7b218eb5ae84d21cb5cb.jpg"
+PHOTO = "https://telegra.ph/file/d1fca1dfe99471c0358e2.jpg"
 
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Kayzu Robot.** \n\n"
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Reyzu Robot.** \n\n"
     TEXT += "♤ **I'm Working Properly** \n\n"
-    TEXT += f"♤ **My Master : [Kayzu](https://t.me/Kayzuuuuu)** \n\n"
+    TEXT += f"♤ **My Master : [Reyzu](https://t.me/Reyzuuu)** \n\n"
     TEXT += f"♤ **Library Version :** `{telever}` \n\n"
     TEXT += f"♤ **Telethon Version :** `{tlhver}` \n\n"
     TEXT += f"♤ **Pyrogram Version :** `{pyrover}` \n\n"
     TEXT += "**Thanks For Adding Me Here ❤️**"
     BUTTON = [
         [
-            Button.url("ʜᴇʟᴘ​", "https://t.me/KayzuMusicBot?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/KayzuSupport"),
+            Button.url("ʜᴇʟᴘ​", "https://t.me/komunitasvirtual?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/komunitas_virtual"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
